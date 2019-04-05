49f9336347ca5cba8a236e8ae4ea6f845ec8abf1
Ofnir01 <emmanuel.torresamaya@gmail.com>
2019-04-05 04:48:18 -0600

    added randInt1to4 cuntion. Geting misterious compiler error probably realted to my own pc.

--- Lab10/Bank.cpp
+++ Lab10/Bank.cpp
@@ -1,6 +1,22 @@
 #include "Bank.h"
 #include <queue>
+#include <random>
+#include <ctime>
 using std::queue;
+
+//***********************************************************
+// Returns an integer 1, 2, 3, or 4 uniformly at random		*
+//***********************************************************
+int randInt1To4() {
+	// shared random number generator (seeded with clock time)
+	static std::default_random_engine randEngine(std::time(0));
+	
+	// shared uniform distribution
+	static std::uniform_int_distribution<int> dist1To4(1, 4);
+	
+	// return a sample
+	return dist1To4(randEngine);
+}
 
 //***************************************
 //CustomerGenerator class definition.	*
@@ -8,7 +24,7 @@ using std::queue;
 
 //CustomerGenerator nextMinute definition.
 void CustomerGenerator::nextMinute() {
-
+	min_to_new_gen = randInt1To4();
 }
 
 //***************************************
@@ -24,3 +40,5 @@ void Bank::nextMinute() {
 void Bank::simulate() {
 
 }
+
+
