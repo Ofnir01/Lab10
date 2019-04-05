5349499737c9c95254ae9428524a9270d49107a6
Emmanuel Torres <emmanuel.torres@ad.snow.edu>
2019-04-04 20:19:11 -0600

    Added some classes and comments describing those classes.

--- Lab10/Bank.cpp
+++ Lab10/Bank.cpp
@@ -1,11 +1,16 @@
 #include "Bank.h"
 
+//
+//Customer Generator definition.
+//
 
 
+//
+//Bank definition.
+//
 Bank::Bank() {
 
 }
-
 
 Bank::~Bank() {
 
--- Lab10/Bank.h
+++ Lab10/Bank.h
@@ -1,5 +1,32 @@
 #ifndef BANK_H_
 #define BANK_H_
+
+//*******************************************************************************************************************
+//Customer structure. This structure will keep track of the following:												*
+//Record the time when the customer arrived in line [as an integer number of minutes from the time bank opened]		*
+//Keep track of how many more minutes of help that customer will require.											*
+//*******************************************************************************************************************
+struct Customer {
+	int timeRecord;
+	int waitTime;
+};
+
+//*******************************************************************************************************************
+//CustomerGenerator class. This class represents the stream of customers arriving at the front door of the bank.	*
+//It will indicate the number of minutes remainding before another customer is generated.							*
+//It will decrement the number of minutes left till the next customer.												*
+//*******************************************************************************************************************
+class CustomerGenerator {
+
+};
+
+//******************************************************************************************************************
+//Bank class. This class will simulate the bank itself. It will have a constant length of a work day. It will		*
+//keep record of the number of minutes since the bank opened. It will have a pointer to the Current customer being	*
+//helped. It will keep track of the line of customers waiting to be helped, a reference to a CustomerGenerator		*
+//object, an integer tracking the maximum queue length seen during the day, and an integer traking the maximum		*
+//wait time.																										*
+//*******************************************************************************************************************
 class Bank
 {
 public:
